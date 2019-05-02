be78c634ced018c897bbd7430680bf3896bfd879
philliplee <ucf4lee@gmail.com>
2019-05-02 13:38:21 -0400

    add comments for 560

--- leetcode/560_Subarray_Sum_Equals_K.py
+++ leetcode/560_Subarray_Sum_Equals_K.py
@@ -16,14 +16,33 @@ def dp(self, nums: List[int], k: int) -> int:
     Runtime: 60 ms, faster than 73.67% of Python3 online submissions for Subarray Sum Equals K.
     Memory Usage: 15.4 MB, less than 11.54% of Python3 online submissions for Subarray Sum Equals K.
     '''
+    # Stores the {sum:count} pair where sum is the cumulated sum found and
+    # count is the number of times the sum had occur.
     pre_sums = {0: 1}
     sum = 0
     counter = 0
 
     for num in nums:
         sum += num
-        # print('sum: ',sum)
-        # print(pre_sums)
+        # Note here, We add the number of times (cumulated_sum-k) had occured
+        # before to the counter. This is because if you subtract k from
+        # current cumulated sum, you get an reduced sum. and if the reduced sum
+        # occured n times before. then we know that there must has n number
+        # of different subarray that sums to (cumulated_sum-k). Then these subarrays
+        # that sums to (cumulated_sum-k) coupled with subarrays that sums to k
+        # would produce n number of subarrays that sums to k.
+        # I.e we have following pseudoarrays that has 2 subarray that sums to (cumulated_sum-k)
+
+        # ARRAY:  |----------------------------------------------------------|
+        # Subarr1:|---- subarr1 sums to (cumulated_sum-k) ---|--------k------|
+
+        # ARRAY:  |----------------------------------------------------------|
+        # Subarr2:|-subarr2 sums to (cumulated_sum-k)-|-----------k----------|
+
+        # Notice subarr1 and subarr2 have different length but they all sum to
+        # (cumulated_sum-k). This means that there must have two different subarray
+        # with different length that sums to k between the last element of subarr1
+        # and subarr2 to current number.
         counter += pre_sums.get(sum - k, 0)
 
         if pre_sums.get(sum):
