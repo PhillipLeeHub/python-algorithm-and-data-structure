85f91e8af6ecb7dd0b8726545400b275e69e95a0
PhillipLeeHub <49844819+PhillipLeeHub@users.noreply.github.com>
2023-01-06 18:07:57 -0500

    Create 205_Isomorphic_Strings_Easy.py

---
+++ leetcode/205_Isomorphic_Strings_Easy.py
@@ -0,0 +1,65 @@
+'''
+205. Isomorphic Strings Easy
+
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+t.length == s.length
+s and t consist of any valid ascii character.
+
+'''
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+
+        '''
+        ascii_map {
+            s.letter : t.letter
+            e: a
+            g: d
+
+        }
+        '''
+        index = 0
+        s_map = {}
+        t_map = {}
+        while(index < len(s)):
+            s_char = s[index]
+            t_char = t[index]
+
+            if s_char in t_map:
+                if t_map[s_char] != t_char:
+                    return False
+            else:
+                t_map[s_char] = t_char
+      
+
+            if t_char in s_map:
+                if s_map[t_char] != s_char:
+                    return False
+            else:
+                s_map[t_char] = s_char
+
+            index+=1
+        return True
